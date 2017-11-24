222d27c828341c3614972dd7fcad5e357d1fb4cf
Sam Dockery <samueldockery@gmail.com>
2017-11-24 12:13:49 -0800

    Tweaks to startup

--- refresh.sh
+++ refresh.sh
@@ -3,3 +3,4 @@ xdotool search --desktop 0 --onlyvisible --name "pi@raspberrypi" windowactivate
 cd /home/pi/projects/PiDash/python
 python3 main.py
 2>/dev/null 1>&2 xdotool search --desktop 0 --onlyvisible --name "Pi Dashboard" windowactivate --sync key --clearmodifiers ctrl+r
+xdotool mousemove 1919 1079
