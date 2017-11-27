dc6ad11e7731ed5d13ec4f0a0c6dec2444727c41
Sam Dockery <samueldockery@gmail.com>
2017-11-26 16:12:59 -0800

    Now refreshes Chrome and goes back to Home

--- refresh.sh
+++ refresh.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 xdotool search --desktop 0 --onlyvisible --name "pi@raspberrypi" windowactivate
+xdotool search --desktop 0 --onlyvisible --name "pi@raspberrypi" windowmove 1261 658
 cd /home/pi/projects/PiDash/python
 python3 main.py
-2>/dev/null 1>&2 xdotool search --desktop 0 --onlyvisible --name "Pi Dashboard" windowactivate --sync key --clearmodifiers ctrl+r
+2>/dev/null 1>&2 xdotool search --desktop 0 --onlyvisible --name "Pi Dashboard" windowactivate --sync key --clearmodifiers alt+Home
 xdotool mousemove 1919 1079
